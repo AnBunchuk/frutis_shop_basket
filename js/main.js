a4d029c69e79b546f861c11c11725204601ac2ff
Bunchuk Andrey <av.bunchuk@gmail.com>
2022-06-17 01:06:41 +0300

    сохранение без комментариев

--- js/main.js
+++ js/main.js
@@ -16,16 +16,11 @@ class Basket {
     // добавляем картинки товаров в магазин
     addPicture() {
         this.myPicture.forEach(v => {
-            // let newDiv = document.createElement('div');
-            // section.append(newDiv);
             let newImg = document.createElement('img');
             newImg.setAttribute('src', `${this.pictureHost}/${v}`);
             newImg.setAttribute('width', '90%');
             newImg.setAttribute('draggable', 'false')
-            // newImg.ondragstart = () => false
             this.sectionSmall.append(newImg);
-
-            this.decart = {};
         })
 
     }
@@ -34,7 +29,7 @@ class Basket {
     mouseDown() {
         document.addEventListener('mousedown', (event) => {
             this.flag = true;
-            this.elementMove = event.target;//присваеем перетягиваемому обьекту картинку на которую нажали
+            this.elementMove = event.target;
         })
     }
     // событие перетягивания
@@ -42,14 +37,9 @@ class Basket {
         document.addEventListener('mousemove', (event) => {
 
             if (this.flag) {
-                // если флаг тру, присваеваем img style обсолютное позиционирование(т.е. убираем из потока)
                 this.elementMove.style.position = 'absolute';
-                // новые координаты элементу img, чтобы двигался вместе с курсором
                 this.elementMove.style.left = event.pageX + 'px';
                 this.elementMove.style.top = event.pageY + 'px';
-                // чтобы курсор был посередине элемента
-                // если оставлять курсор в том месте где нажали, 
-                // то доп вычитать координаты первичного положения курсора
                 this.elementMove.style.transform = 'translate(-50%, -50%)';
             }
         })
@@ -58,24 +48,19 @@ class Basket {
     mouseUp() {
         document.addEventListener('mouseup', (event) => {
             this.flag = false;
-            // координаты области в какой при попадании в которую элемент остается в новом месте
             if (event.pageX > this.basketDecart.left && event.pageX < this.basketDecart.right
                 && event.pageY > this.basketDecart.top && event.pageY < this.basketDecart.bottom) {
-                // добавляю перетаскиваемый обьект в существующую разметку big
+
                 this.sectionBig.append(this.elementMove)
-                // удаляю атрибут style с позиционированием и координатами из img
                 this.elementMove.removeAttribute('style')
             }
-            // координаты области в какой при попадании в которую элемент остается в новом месте
             if (event.pageX > this.shelfDecart.left && event.pageX < this.shelfDecart.right
                 && event.pageY > this.shelfDecart.top && event.pageY < this.shelfDecart.bottom) {
-                // добавляю перетаскиваемый обьект в small
+
                 this.sectionSmall.append(this.elementMove)
-                // удаляю атрибут style с позиционированием и координатами из img
                 this.elementMove.removeAttribute('style')
             } 
             else {
-                // если условие не выполнено элемент вернулся в исходное положение
                 this.elementMove.removeAttribute('style')
             }
             this.calcBasket()
